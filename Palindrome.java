12f126fb1cec2b3f23c4f0192715e8ab4061ee17
vinayak7777 <65407554+vinayak7777@users.noreply.github.com>
2022-10-19 12:59:27 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Palindrome {
+	
+	public static void main (String[] args) {
+		boolean res=true;
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter a string to check whether it is a palindrome:");
+		String s1=s.nextLine();
+		int l=s1.length();
+		for(int i=0;i<=l/2;i++)
+		{
+			if(s1.charAt(i)!=s1.charAt(l-i-1))
+			{
+				res=false;
+				break;
+			}
+		}
+		if(res==true)
+		System.out.println(s1+" is a palindrome");
+		else
+		System.out.println(s1+" is not a palindrome");
+	}
+}
+
